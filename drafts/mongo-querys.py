943147490a599b3c08881cd7465ecc20e60ec095
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2017-06-25 21:16:35 +0200

    Añadidos archivos de Materialize.css

--- drafts/mongo-querys.py
+++ drafts/mongo-querys.py
@@ -1,13 +1,37 @@
 from pymongo import MongoClient
 
-client = MongoClient()
-database = client.glaza
-collection = database.facts
+CLIENT = MongoClient()
+DATABASE = CLIENT.glaza
+COLLECTION = DATABASE.facts
 
-# Get unique macaddresses
-result = collection.distinct( "ansible_facts.ansible_default_ipv4.macaddress" )
-print(result)
+def querys():
+    """Executes test mongo querys."""
 
-# Get last JSON of a macaddress
-result = collection.find( "ansible_facts.ansible_default_ipv4.macaddress": "94:de:80:d1:64:0d" )
-print(result)
+    # Get all documents
+    # result = COLLECTION.find({})
+    # show(result)
+
+    # Get unique macaddresses
+    print("Different MAC addresses:")
+    result = COLLECTION.distinct("ansible_facts.ansible_default_ipv4.macaddress")
+    print(result)
+    print()
+
+    # Get last JSON of a macaddress
+    print("Documents with MAC 94:de:80:d1:64:0d")
+    result = COLLECTION.find({"ansible_facts.ansible_default_ipv4.macaddress": "94:de:80:d1:64:0d"})
+    show(result)
+    print()
+
+
+def show(cursor):
+    """Shows cursor's contents."""
+
+    for doc in cursor:
+        print(doc)
+
+    print("\n")
+
+
+
+querys()
