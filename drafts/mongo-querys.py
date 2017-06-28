e5bd9719c56425c1da4d7734459999b11a19bc78
Victor Carceler <vcarceler@iespuigcastellar.xeill.net>
2017-06-28 23:35:24 +0200

    Aggregated querys.

--- drafts/mongo-querys.py
+++ drafts/mongo-querys.py
@@ -25,12 +25,33 @@ def querys():
 
     # Get specified fields
     print("especified fields of 1c:c1:de:50:21:12")
-    result = COLLECTION.find({"ansible_facts.ansible_default_ipv4.macaddress": "1c:c1:de:50:21:12"},
+    result = COLLECTION.find(
+        {"ansible_facts.ansible_default_ipv4.macaddress": "1c:c1:de:50:21:12"},
         {
             "ansible_facts.ansible_default_ipv4.macaddress": "1",
             "ansible_facts.ansible_date_time.iso8601": "1",
             "ansible_facts.ansible_devices.sda.size": "1",
+            "ansible_facts.ansible_system_vendor": "1",
         })
+    show(result)
+    print()
+
+    # Count RAM size from a network
+    NETWORK = "192.168.17.0"
+    print("Aggregated RAM of network {}".format(NETWORK))
+    result = COLLECTION.aggregate([
+        {"$match": {"ansible_facts.ansible_default_ipv4.network": "{}".format(NETWORK)}},
+        {"$group": {"_id": "$ansible_facts.ansible_memtotal_mb", "count": {"$sum": 1}}},
+    ])
+    show(result)
+    print()
+
+    # Count DISK size from a network
+    print("Aggregated DISK of network {}".format(NETWORK))
+    result = COLLECTION.aggregate([
+        {"$match": {"ansible_facts.ansible_default_ipv4.network": "{}".format(NETWORK)}},
+        {"$group": {"_id": "$ansible_facts.ansible_devices.sda.size", "count": {"$sum": 1}}},
+    ])
     show(result)
     print()
 
